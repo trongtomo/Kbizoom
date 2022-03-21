2684da69ddec6a64e5fb4e26af6182f059e25d82
trongtomo <binguyenquoctrong@gmail.com>
2022-03-21 14:44:13 +0700

    #9

---
+++ header.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>css/style.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/node_modules/bootstrap/dist/css/bootstrap.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/node_modules/bootstrap/scss/bootstrap.scss.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/node_modules/bootstrap/scss/_variables.scss" type="text/css" media="all" />
+    <?php wp_head();?>
+    
+  </head>
+  <body class="bg-body">
+    <!-- Header-->
+    <header>
+      <div class="container">
+        <nav
+          class="navbar navbar-expand-lg navbar-light bg-light shadow p-3 mb-5 bg-body rounded-bottom rounded-3 border-bottom-0"
+        >
+          <a class="navbar-brand" href="#">KBizoom</a>
+          <div
+            class="collapse navbar-collapse position-relative"
+            id="navbarSupportedContent"
+          >
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item position-relative">
+                <a
+                  class="nav-link active text-primary fw-bolder"
+                  aria-current="page"
+                  href="#"
+                  >Home</a
+                >
+              </li>
+              <li class="nav-item position-relative">
+                <a class="nav-link disabled fw-normal" href="#">News</a>
+              </li>
+              <li class="nav-item position-relative">
+                <a class="nav-link disabled fw-normal" href="#">Netizen</a>
+              </li>
+              <li class="nav-item position-relative">
+                <a class="nav-link disabled fw-normal" href="#">Quiz</a>
+              </li>
+              <li class="nav-item position-relative">
+                <a class="nav-link disabled fw-normal" href="#">Videos</a>
+              </li>
+              <li class="nav-item position-relative">
+                <a class="nav-link disabled fw-normal" href="#">Photo</a>
+              </li>
+              <li class="nav-item position-relative">
+                <a class="nav-link disabled fw-normal" href="#">Poll</a>
+              </li>
+            </ul>
+            <form class="d-flex" id="search">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search..."
+                aria-label="Search"
+                style="
+                  border-radius: 300px;
+                  border-style: solid 10px;
+                  border-color: purple;
+                "
+              />
+            </form>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- End of Header-->
+    <main>
+     <p>this is Main</p>
